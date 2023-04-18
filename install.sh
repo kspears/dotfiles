67ec00b512fc7b943cf88552e75bd4ed15915cdc
Kevin Spears <kevin@thespears.com>
2023-04-18 15:05:52 -0700

    added zshrc and added loop for install script

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-#install .dotfiles
-echo "creating symlinks for dotfiles"
-ln -rfs ./tmux/tmux.conf ~/.tmux.conf
-ln -rfs ./git/gitconfig ~/.gitconfig
+# Kevin Spears
+# 2023-04-16
+
+# Array of dotfiles to install
+declare -A dotfiles
+dotfiles[.tmux.conf]=./tmux/tmux.conf
+dotfiles[.gitconfig]=./git/gitconfig
+dotfiles[.zshrc]=./zshrc/zshrc
+
+#loop through files to install configs
+for key in "${!dotfiles[@]}"; do
+	echo "Installing ${dotfiles[$key]}"
+	ln -rfs ${dotfiles[$key]} ~/$key
+done
+
+#if needed install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then 
+	echo "Installing oh-my-zsh"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+
