6e9d81a40b2009e97373e3f2caee2068472ea0a6
Kevin Spears <kevin@thespears.com>
2026-02-21 07:26:03 -0800

    Set up tmux and neovim for nvim + claude + terminal workflow
    - Rewrite tmux config: true color for Kitty, vim-tmux-navigator, vi copy
      mode, TokyoNight status bar, TPM
    - Add tmux aliases (tl, ta, tk, tn) to aliases.zsh
    - Add vim-tmux-navigator plugin to neovim

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -21,3 +21,9 @@ alias k='kubectl'
 alias d='dirs -v'
 
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
+
+# tmux
+alias tl='tmux list-sessions'
+alias ta='tmux attach -t'
+alias tk='tmux kill-session -t'
+alias tn='tmux new-session -s'
