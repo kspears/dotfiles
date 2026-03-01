78d474e13c2339c0e1860ce602c5c0af34e10ed3
Kevin Spears <kevin@thespears.com>
2026-02-28 20:56:37 -0800

    Add stale starship.toml symlink to cleanup script

--- .chezmoiscripts/run_once_cleanup-old-symlinks.sh
+++ .chezmoiscripts/run_once_cleanup-old-symlinks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Remove broken symlinks left behind by old install.sh
-for f in "$HOME/.vimrc" "$HOME/prompt" "$HOME/aliases_pf.zsh"; do
+for f in "$HOME/.vimrc" "$HOME/prompt" "$HOME/aliases_pf.zsh" "$HOME/.config/starship.toml"; do
   [ -L "$f" ] && rm "$f" || true
 done
