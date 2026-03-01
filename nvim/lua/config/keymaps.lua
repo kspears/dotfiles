42111e6fc4b6ce92f893d4a0d00f33fbf897327c
Kevin Spears <kevin@thespears.com>
2026-02-28 16:37:19 -0800

    Replace LazyVim with zero-plugin native Neovim 0.11 config
    Drop all plugins and lazy.nvim in favor of built-in LSP, completion,
    treesitter, and diagnostics. Custom implementations for git signs,
    statusline, fzf floating window, tmux-aware navigation, and ayu-dark
    colorscheme. LSP configs for ruff, terraform-ls, ts_ls, and lua_ls.

--- nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-
--- <C-p> as alias for find files (LazyVim uses <leader>ff)
-vim.keymap.set("n", "<C-p>", function() require("telescope.builtin").find_files() end, { desc = "Find Files" })
