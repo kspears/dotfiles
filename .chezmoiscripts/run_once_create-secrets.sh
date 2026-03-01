16c53cc7fe5011a76d896355367e016700593630
Kevin Spears <kevin@thespears.com>
2026-02-28 20:51:34 -0800

    Handle broken symlink in secrets.zsh creation script

--- .chezmoiscripts/run_once_create-secrets.sh
+++ .chezmoiscripts/run_once_create-secrets.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Ensure secrets.zsh exists (sourced by .zshrc, never tracked)
+# Remove broken symlink from old install.sh if present
+[ -L "$HOME/secrets.zsh" ] && rm "$HOME/secrets.zsh"
 [ -f "$HOME/secrets.zsh" ] || touch "$HOME/secrets.zsh"
